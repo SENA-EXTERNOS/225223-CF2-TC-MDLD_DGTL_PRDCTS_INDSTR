4a9745cbb8a41e8a0920b0d3041572c4008447f1
elrilacho <80546125+elrilacho@users.noreply.github.com>
2022-10-03 15:32:22 -0500

    sintesis

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -3,15 +3,15 @@
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    p Con el diagrama de actividades se termina la descripción de las tres técnicas usadas para la especificación y el análisis de requisitos, como se pudo analizar cada una de las técnicas nos brinda características para lograr representar gráficamente y de una forma clara, los requisitos que un usuario necesita poder cumplir satisfactoriamente con las solicitudes de los clientes y usuarios, por otro lado se pueden usar todas las técnicas en una especificación y análisis de requisitos si se ve la necesidad.
-    p.mb-5 A continuación, se muestra un mapa conceptual con los elementos más importantes desarrollados en este componente.
-
+    .row.justify-content-center.mb-4
+      .col-lg-10
+        p ¿Se imagina cómo sería el desarrollo industrial si no existieran herramientas digitales de diseño y fabricación? ¿Cuánto tiempo llevaría la ejecución de grandes obras de infraestructura sin la ayuda de softwares que realicen cálculos de forma rápida y precisa? Las herramientas CAD han apalancado el desarrollo de diferentes industrias, por esto, mientras más evolucionen, más rápido crecerán las industrias.
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
           img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
       .col-auto
-        a.anexo.mb-4(:href="obtenerLink('/downloads/prueba.pdf')" target="_blank")
+        a.anexo.mb-4(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
           .anexo__icono
             img(src="@/assets/template/icono-pdf.svg")
           .anexo__texto
